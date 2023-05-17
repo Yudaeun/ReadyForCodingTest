20a02869793e6f2ea2bfe3607a670307a35e10b2
YUDAEUN <54846663+Yudaeun@users.noreply.github.com>
2023-05-17 20:59:09 +0900

    [unrated] Title: 특정 옵션이 포함된 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++ 프로그래머스/unrated/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT CAR_ID,CAR_TYPE,DAILY_FEE,OPTIONS
-FROM CAR_RENTAL_COMPANY_CAR
-WHERE OPTIONS LIKE '%네비게이션%'
-ORDER BY CAR_ID DESC;
+SELECT *
+from car_rental_company_car
+where options like '%네비게이션%'
+order by car_id desc;
