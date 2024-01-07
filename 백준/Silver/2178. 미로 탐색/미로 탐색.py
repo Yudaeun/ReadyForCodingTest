cc471758727eb4db8e5691a526053afaf3a70ddd
YUDAEUN <54846663+Yudaeun@users.noreply.github.com>
2024-01-07 15:37:44 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,29 @@
 from collections import deque
 
 n,m=map(int,input().split())
-graph=[]
+map=[]
 
 for _ in range(n):
-    graph.append(list(map(int,input())))
+    map.append(list(str(input())))
 
-
-
+visited=[[0]*m for _ in range(n)]
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-visited=[[0 for _ in range(m)] for _ in range(n)]
-def bfs(x,y):
+
+def bfs(startX,startY):
+    visited[startX][startY]=1
     q=deque()
-    q.append((x,y))
+    q.append((startX,startY))
+
     while q:
-        x,y=q.popleft()
+        cx,cy=q.popleft()
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if nx<=-1 or nx>=n or ny<=-1 or ny>=m:
+            nx,ny=cx+dx[i],cy+dy[i]
+            if nx<0 or ny<0 or nx>=n or ny>=m:
                 continue
-            if graph[nx][ny]==0:
-                continue
-            if graph[nx][ny]>=1 and visited[nx][ny]==0:
-                graph[nx][ny]=graph[x][y]+1
-                visited[nx][ny]=1
+            if map[nx][ny]=='1' and visited[nx][ny]==0:
+                visited[nx][ny]=visited[cx][cy]+1
                 q.append((nx,ny))
-bfs(0,0)
-print(graph[n-1][m-1])
+    return visited[n-1][m-1]
+
+print(bfs(0,0))
