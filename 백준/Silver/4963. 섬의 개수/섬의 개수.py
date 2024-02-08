04964bb9f3ea1f6aab61348f03da1a10e47c62ce
YUDAEUN <54846663+Yudaeun@users.noreply.github.com>
2024-02-08 21:06:37 +0900

    [Silver II] Title: 섬의 개수, Time: 96 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,45 +1,39 @@
+import sys
 from collections import deque
-dx=[-1,-1,-1,0,0,1,1,1]
-dy=[-1,0,1,-1,1,-1,0,1]
 
-'''
-def dfs(x,y,w,h):
-    graph[x][y]=0
-    for i in range(8):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if nx<=-1 or nx>=w or ny<=-1 or ny>=h:
-            continue
-        if graph[nx][ny]==0:
-            continue
-        if graph[nx][ny]==1:
-            dfs(nx,ny,w,h)
-'''
-def bfs(x,y,w,h):
+dx=[-1,1,0,0,-1,-1,1,1]
+dy=[0,0,-1,1,1,-1,-1,1]
+
+def bfs(start):
     q=deque()
-    q.append((x,y))
+    q.append(start)
+    visited[start[0]][start[1]]=1
+
     while q:
-        x,y=q.popleft()
+        cx,cy=q.popleft()
         for i in range(8):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if nx<=-1 or nx>=h or ny<=-1 or ny>=w:
-                continue
-            if graph[nx][ny]==1:
-                graph[nx][ny]=0
-                q.append((nx,ny))
+            nx,ny=cx+dx[i],cy+dy[i]
+            if 0<=nx<h and 0<=ny<w:
+                if visited[nx][ny]!=1 and graph[nx][ny]==1:
+                    visited[nx][ny]=1
+                    q.append([nx,ny])
 
 while True:
     w,h=map(int,input().split())
     if w==0 and h==0:
         break
+
     graph=[]
-    cnt=0
     for _ in range(h):
         graph.append(list(map(int,input().split())))
-    for x in range(h):
-        for y in range(w):
-            if graph[x][y]==1:
-                bfs(x,y,w,h)
-                cnt+=1
-    print(cnt)
+
+    visited = [[0] * w for _ in range(h)]
+    answer=0
+
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j]==1 and visited[i][j]==0:
+                bfs([i,j])
+                answer+=1
+
+    print(answer)
