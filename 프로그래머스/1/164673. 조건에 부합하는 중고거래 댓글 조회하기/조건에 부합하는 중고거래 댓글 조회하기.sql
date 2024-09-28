c75849a1e21fc15ce7afd1a96762828feba5d186
YUDAEUN <54846663+Yudaeun@users.noreply.github.com>
2024-09-28 13:09:13 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,4 @@
+SELECT A.title,A.board_id,B.reply_id, B.writer_id,B.contents,date_format(B.created_date,'%Y-%m-%d') created_date
+from used_goods_board A join used_goods_reply B on A. board_id=B.board_id
+where date_format(A.created_date,'%Y-%m')='2022-10'
+order by B.created_date,A.title
