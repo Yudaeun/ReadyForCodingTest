0b6ca723648765e992daf1087c53945a505d2498
YUDAEUN <54846663+Yudaeun@users.noreply.github.com>
2025-10-25 23:21:44 +0900

    [level 2] Title: 더 맵게, Time: 1689.07 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,32 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-            int answer = 0;
-            PriorityQueue<Integer> pq=new PriorityQueue<>();
-            int firstMinNum=-1;
-            int secondMinNum=-1;
-            int scovilleScore=0;
-
-            for(int i=0;i<scoville.length;i++){
-                pq.add(scoville[i]);
-            }
-
-            while(!pq.isEmpty()){
-                firstMinNum=pq.poll();
-                if(firstMinNum>=K){
-                    break;
-                }else{
-                    if(pq.size()<=0){
-                        answer=-1;
-                        break;
-                    }
-                    secondMinNum=pq.poll();
-                    scovilleScore=firstMinNum+(secondMinNum*2);
-                    pq.add(scovilleScore);
-                    answer++;
-                }
-            }
-
-            return answer;
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.add(s);
         }
+        
+        while (!pq.isEmpty()) {
+            int sco1 = pq.poll();
+            
+            if (sco1 >= K) {
+                return answer;
+            }
+            
+            if (pq.isEmpty()) {
+                return -1;
+            }
+            
+            int sco2 = pq.poll();
+            int newSco = sco1 + (sco2 * 2);
+            pq.add(newSco);
+            answer++;
+            
+            
+            
+        }
+        
+        
+        return answer;
+    }
 }
